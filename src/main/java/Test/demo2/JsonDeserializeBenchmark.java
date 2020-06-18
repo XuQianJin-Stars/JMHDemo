fe7dc8da58faf5d12603dba701a66b125fa78e43
XuQianJin-Stars <forwardxu@apache.com>
2020-06-18 15:30:10 +0800

    add stringAdd && StringBuilder test

--- src/main/java/Test/demo2/JsonDeserializeBenchmark.java
+++ src/main/java/Test/demo2/JsonDeserializeBenchmark.java
@@ -26,58 +26,57 @@ import java.util.concurrent.TimeUnit;
 @OutputTimeUnit(TimeUnit.SECONDS)
 @State(Scope.Benchmark)
 public class JsonDeserializeBenchmark {
-    /**
-     * 反序列化次数参数
-     */
-    @Param({"1000", "10000", "100000", "500000"})
-    private int count;
+  /** 反序列化次数参数 */
+  @Param({"1000", "10000", "100000", "500000"})
+  private int count;
 
-    private String jsonStr;
+  private String jsonStr;
 
-    public static void main(String[] args) throws Exception {
-        Options opt = new OptionsBuilder()
-                .include(JsonDeserializeBenchmark.class.getSimpleName())
-                .forks(1)
-                .warmupIterations(0)
-                .build();
-        Collection<RunResult> results = new Runner(opt).run();
-        ResultExporter.exportResult("JSON反序列化性能", results, "count", "秒");
+  public static void main(String[] args) throws Exception {
+    Options opt =
+        new OptionsBuilder()
+            .include(JsonDeserializeBenchmark.class.getSimpleName())
+            .forks(1)
+            .warmupIterations(0)
+            .build();
+    Collection<RunResult> results = new Runner(opt).run();
+    ResultExporter.exportResult("JSON反序列化性能", results, "count", "秒");
+  }
+
+  @Benchmark
+  public void JsonLib() {
+    for (int i = 0; i < count; i++) {
+      JsonLibUtil.json2Bean(jsonStr, Person.class);
     }
+  }
 
-    @Benchmark
-    public void JsonLib() {
-        for (int i = 0; i < count; i++) {
-            JsonLibUtil.json2Bean(jsonStr, Person.class);
-        }
+  @Benchmark
+  public void Gson() {
+    for (int i = 0; i < count; i++) {
+      GsonUtil.json2Bean(jsonStr, Person.class);
     }
+  }
 
-    @Benchmark
-    public void Gson() {
-        for (int i = 0; i < count; i++) {
-            GsonUtil.json2Bean(jsonStr, Person.class);
-        }
+  @Benchmark
+  public void FastJson() {
+    for (int i = 0; i < count; i++) {
+      FastJsonUtil.json2Bean(jsonStr, Person.class);
     }
+  }
 
-    @Benchmark
-    public void FastJson() {
-        for (int i = 0; i < count; i++) {
-            FastJsonUtil.json2Bean(jsonStr, Person.class);
-        }
+  @Benchmark
+  public void Jackson() {
+    for (int i = 0; i < count; i++) {
+      JacksonUtil.json2Bean(jsonStr, Person.class);
     }
+  }
 
-    @Benchmark
-    public void Jackson() {
-        for (int i = 0; i < count; i++) {
-            JacksonUtil.json2Bean(jsonStr, Person.class);
-        }
-    }
+  @Setup
+  public void prepare() {
+    jsonStr =
+        "{\"name\":\"邵同学\",\"fullName\":{\"firstName\":\"zjj_first\",\"middleName\":\"zjj_middle\",\"lastName\":\"zjj_last\"},\"age\":24,\"birthday\":null,\"hobbies\":[\"篮球\",\"游泳\",\"coding\"],\"clothes\":{\"shoes\":\"安踏\",\"trousers\":\"adidas\",\"coat\":\"Nike\"},\"friends\":[{\"name\":\"小明\",\"fullName\":{\"firstName\":\"xxx_first\",\"middleName\":\"xxx_middle\",\"lastName\":\"xxx_last\"},\"age\":24,\"birthday\":null,\"hobbies\":[\"篮球\",\"游泳\",\"coding\"],\"clothes\":{\"shoes\":\"安踏\",\"trousers\":\"adidas\",\"coat\":\"Nike\"},\"friends\":null},{\"name\":\"Tony\",\"fullName\":{\"firstName\":\"xxx_first\",\"middleName\":\"xxx_middle\",\"lastName\":\"xxx_last\"},\"age\":24,\"birthday\":null,\"hobbies\":[\"篮球\",\"游泳\",\"coding\"],\"clothes\":{\"shoes\":\"安踏\",\"trousers\":\"adidas\",\"coat\":\"Nike\"},\"friends\":null},{\"name\":\"陈小二\",\"fullName\":{\"firstName\":\"xxx_first\",\"middleName\":\"xxx_middle\",\"lastName\":\"xxx_last\"},\"age\":24,\"birthday\":null,\"hobbies\":[\"篮球\",\"游泳\",\"coding\"],\"clothes\":{\"shoes\":\"安踏\",\"trousers\":\"adidas\",\"coat\":\"Nike\"},\"friends\":null}]}";
+  }
 
-    @Setup
-    public void prepare() {
-        jsonStr = "{\"name\":\"邵同学\",\"fullName\":{\"firstName\":\"zjj_first\",\"middleName\":\"zjj_middle\",\"lastName\":\"zjj_last\"},\"age\":24,\"birthday\":null,\"hobbies\":[\"篮球\",\"游泳\",\"coding\"],\"clothes\":{\"shoes\":\"安踏\",\"trousers\":\"adidas\",\"coat\":\"Nike\"},\"friends\":[{\"name\":\"小明\",\"fullName\":{\"firstName\":\"xxx_first\",\"middleName\":\"xxx_middle\",\"lastName\":\"xxx_last\"},\"age\":24,\"birthday\":null,\"hobbies\":[\"篮球\",\"游泳\",\"coding\"],\"clothes\":{\"shoes\":\"安踏\",\"trousers\":\"adidas\",\"coat\":\"Nike\"},\"friends\":null},{\"name\":\"Tony\",\"fullName\":{\"firstName\":\"xxx_first\",\"middleName\":\"xxx_middle\",\"lastName\":\"xxx_last\"},\"age\":24,\"birthday\":null,\"hobbies\":[\"篮球\",\"游泳\",\"coding\"],\"clothes\":{\"shoes\":\"安踏\",\"trousers\":\"adidas\",\"coat\":\"Nike\"},\"friends\":null},{\"name\":\"陈小二\",\"fullName\":{\"firstName\":\"xxx_first\",\"middleName\":\"xxx_middle\",\"lastName\":\"xxx_last\"},\"age\":24,\"birthday\":null,\"hobbies\":[\"篮球\",\"游泳\",\"coding\"],\"clothes\":{\"shoes\":\"安踏\",\"trousers\":\"adidas\",\"coat\":\"Nike\"},\"friends\":null}]}";
-    }
-
-    @TearDown
-    public void shutdown() {
-    }
+  @TearDown
+  public void shutdown() {}
 }
